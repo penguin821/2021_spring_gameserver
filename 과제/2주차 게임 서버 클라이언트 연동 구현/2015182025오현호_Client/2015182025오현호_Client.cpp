57f14a200808e630ca7dc7e9df1c539c874b60d1
Oh Hyeon Ho <penguin821@naver.com>
2021-03-29 19:14:39 +0900

    finish overlapped connect

---
+++ 과제/2주차 게임 서버 클라이언트 연동 구현/2015182025오현호_Client/2015182025오현호_Client.cpp
@@ -0,0 +1,412 @@
+﻿// 2015182025오현호_Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "2015182025오현호_Client.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+string client_ip;
+SOCKET s_socket;
+constexpr short PORT = 9000;
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void error_display(const char* msg, int err_no)
+{
+    WCHAR* lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, err_no,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    cout << msg;
+    wcout << L"에러 " << lpMsgBuf << endl;
+    LocalFree(lpMsgBuf);
+}
+struct Vec2
+{
+    short x;
+    short y;
+};
+
+struct Key
+{
+    char key;
+};
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+    GdiplusStartupInput         m_GdiplusStartupInput;
+    ULONG_PTR                   m_GdiplusToken;
+    GdiplusStartup(&m_GdiplusToken, &m_GdiplusStartupInput, NULL);
+
+    WSADATA wsadata;
+    WSAStartup(MAKEWORD(2, 0), &wsadata);
+    s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, 0, 0, 0);
+    wcout.imbue(std::locale("korean"));
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY2015182025CLIENT, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY2015182025CLIENT));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY2015182025CLIENT));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY2015182025CLIENT);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, 495, 539, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    Vec2 pos;
+    Key input_key;
+    static short PieceX = 0, PieceY = 0;
+    switch (message)
+    {
+    case WM_CHAR:
+        if (wParam == VK_RETURN && !client_ip.empty())
+        {
+            SOCKADDR_IN server_a;
+            ZeroMemory(&server_a, sizeof(server_a));
+            server_a.sin_family = AF_INET;
+            server_a.sin_port = htons(PORT);
+            inet_pton(AF_INET, client_ip.c_str(), &server_a.sin_addr);
+
+            int retval = WSAConnect(s_socket, reinterpret_cast<SOCKADDR*>(&server_a), sizeof(server_a), 0, 0, 0, 0);
+            if (SOCKET_ERROR == retval)
+            {
+                error_display("connect from cilent", WSAGetLastError());
+                exit(-1);
+            }
+            else
+            {
+                WSABUF r_wsabuf;
+                r_wsabuf.buf = reinterpret_cast<char*>(&pos);
+                r_wsabuf.len = sizeof(pos);
+                DWORD recv_bytes;
+                DWORD r_flag = 0;
+                int retval = WSARecv(s_socket, &r_wsabuf, 1, &recv_bytes, &r_flag, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("recv from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+                PieceX = pos.x;
+                PieceY = pos.y;
+            }
+        }
+        else if (wParam == VK_BACK)
+        {
+            if (!client_ip.empty())
+                client_ip.pop_back();
+        }
+        else
+        {
+            client_ip.push_back(wParam);
+        }
+        break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+            WSABUF s_wsabuf;
+            DWORD send_bytes;
+        case VK_UP:
+            if (PieceY == 0)
+                break;
+            else
+            {
+                input_key.key = 'w';
+                s_wsabuf.buf = reinterpret_cast<char*>(&input_key.key);
+                s_wsabuf.len = sizeof(input_key);
+                int retval = WSASend(s_socket, &s_wsabuf, 1, &send_bytes, 0, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("send from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+
+                WSABUF r_wsabuf;
+                r_wsabuf.buf = reinterpret_cast<char*>(&pos);
+                r_wsabuf.len = sizeof(pos);
+                DWORD recv_bytes;
+                DWORD r_flag = 0;
+                retval = WSARecv(s_socket, &r_wsabuf, 1, &recv_bytes, &r_flag, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("recv from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+                PieceX = pos.x;
+                PieceY = pos.y;
+            }
+            break;
+        case VK_DOWN:
+            if (PieceY == 420)
+                break;
+            else
+            {
+                input_key.key = 's';
+                s_wsabuf.buf = reinterpret_cast<char*>(&input_key.key);
+                s_wsabuf.len = sizeof(input_key);
+                int retval = WSASend(s_socket, &s_wsabuf, 1, &send_bytes, 0, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("send from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+
+                WSABUF r_wsabuf;
+                r_wsabuf.buf = reinterpret_cast<char*>(&pos);
+                r_wsabuf.len = sizeof(pos);
+                DWORD recv_bytes;
+                DWORD r_flag = 0;
+                retval = WSARecv(s_socket, &r_wsabuf, 1, &recv_bytes, &r_flag, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("recv from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+                PieceX = pos.x;
+                PieceY = pos.y;
+            }
+            break;
+        case VK_LEFT:
+            if (PieceX == 0)
+                break;
+            else
+            {
+                input_key.key = 'a';
+                s_wsabuf.buf = reinterpret_cast<char*>(&input_key.key);
+                s_wsabuf.len = sizeof(input_key);
+                int retval = WSASend(s_socket, &s_wsabuf, 1, &send_bytes, 0, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("send from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+
+                WSABUF r_wsabuf;
+                r_wsabuf.buf = reinterpret_cast<char*>(&pos);
+                r_wsabuf.len = sizeof(pos);
+                DWORD recv_bytes;
+                DWORD r_flag = 0;
+                retval = WSARecv(s_socket, &r_wsabuf, 1, &recv_bytes, &r_flag, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("recv from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+                PieceX = pos.x;
+                PieceY = pos.y;
+            }
+            break;
+        case VK_RIGHT:
+            if (PieceX == 420)
+                break;
+            else
+            {
+                input_key.key = 'd';
+                s_wsabuf.buf = reinterpret_cast<char*>(&input_key.key);
+                s_wsabuf.len = sizeof(input_key);
+                int retval = WSASend(s_socket, &s_wsabuf, 1, &send_bytes, 0, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("send from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+
+                WSABUF r_wsabuf;
+                r_wsabuf.buf = reinterpret_cast<char*>(&pos);
+                r_wsabuf.len = sizeof(pos);
+                DWORD recv_bytes;
+                DWORD r_flag = 0;
+                retval = WSARecv(s_socket, &r_wsabuf, 1, &recv_bytes, &r_flag, 0, 0);
+                if (SOCKET_ERROR == retval)
+                {
+                    error_display("recv from client", WSAGetLastError());
+                    exit(-1);
+                }
+
+                PieceX = pos.x;
+                PieceY = pos.y;
+            }
+            break;
+        }
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            wstring input_ip = L"Server IP : ";
+            wstring inpuy_ip2{ client_ip.begin(),client_ip.end() };
+            wstring textBox = input_ip + inpuy_ip2;
+
+            Graphics graphics(hdc);
+            Image image(L"chessboard.bmp");
+
+            graphics.DrawImage(&image, 0, 0);
+
+            Ellipse(hdc, 0 + PieceX, 0 + PieceY, 60 + PieceX, 60 + PieceY);
+            RECT rt = { 100,100,400,300 };
+            DrawText(hdc, textBox.c_str(), -1, &rt, DT_CENTER | DT_WORDBREAK);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        closesocket(s_socket);
+        WSACleanup();
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ 과제/3주차 다중 접속 서버 구현/overlapped_server/overlapped_server/server.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <map>
+using namespace std;
+#include <WS2tcpip.h>
+#include <ctime>
+#pragma comment(lib, "Ws2_32.lib")
+
+constexpr int MAX_BUFFER = 1024;
+constexpr short SERVER_PORT = 3500;
+
+struct Vec2
+{
+	short x;
+	short y;
+};
+
+struct SESSION // 세션
+{
+	WSAOVERLAPPED overlapped; // 얘를 맨 위로하면 세션의 주소랑 같아짐
+	// 구조체의 첫 맴버 변수와 구조체 변수의 주소값은 같아 = C 표준
+	WSABUF dataBuffer;
+	SOCKET socket;
+
+	Vec2 pos;
+	char dir;
+};
+
+map <SOCKET, SESSION> clients;
+
+void CALLBACK recv_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags);
+void CALLBACK send_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags);
+
+void CALLBACK recv_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags)
+{
+	SOCKET client_s = reinterpret_cast<SESSION*>(overlapped)->socket; // 이렇게 소켓 정보 읽어옴
+
+	if (dataBytes == 0) // 읽은 데이터 량
+	{
+		closesocket(clients[client_s].socket);
+		clients.erase(client_s);
+		return;
+	}  // 클라이언트가 closesocket을 했을 경우
+	cout << "From client [" << client_s << "] direction = " << clients[client_s].dir << endl;
+	clients[client_s].dataBuffer.len = dataBytes;
+
+	switch (clients[client_s].dir)
+	{
+	case 'w':
+	case 'W':
+		if (0 != clients[client_s].pos.y)
+			clients[client_s].pos.y -= 60;
+		break;
+	case 's':
+	case 'S':
+		if (7 != clients[client_s].pos.y)
+			clients[client_s].pos.y += 60;
+		break;
+	case 'a':
+	case 'A':
+		if (0 != clients[client_s].pos.x)
+			clients[client_s].pos.x -= 60;
+		break;
+	case 'd':
+	case 'D':
+		if (7 != clients[client_s].pos.x)
+			clients[client_s].pos.x += 60;
+		break;
+	}
+
+	memset(&(clients[client_s].overlapped), 0, sizeof(WSAOVERLAPPED));
+	Vec2 n_pos = clients[client_s].pos;
+	clients[client_s].dataBuffer.buf = reinterpret_cast<char*>(&n_pos); // 한 세션 생성 끝
+	clients[client_s].dataBuffer.len = sizeof(clients[client_s].pos);
+	WSASend(client_s, &(clients[client_s].dataBuffer), 1, NULL, 0, overlapped, send_callback);
+}
+
+void CALLBACK send_callback(DWORD Error, DWORD dataBytes, LPWSAOVERLAPPED overlapped, DWORD lnFlags)
+{
+	SOCKET client_s = reinterpret_cast<SESSION*>(overlapped)->socket;
+
+	if (dataBytes == 0)
+	{
+		closesocket(clients[client_s].socket);
+		clients.erase(client_s);
+		return;
+	}  // 클라이언트가 closesocket을 했을 경우
+
+	// WSASend(응답에 대한)의 콜백일 경우
+	cout << "TRACE - Send to " << client_s << "/ position : " << clients[client_s].pos.x << " , " << clients[client_s].pos.y << endl;
+
+	memset(&(clients[client_s].overlapped), 0, sizeof(WSAOVERLAPPED));
+	clients[client_s].dataBuffer.buf = reinterpret_cast<char*>(&clients[client_s].dir);
+	clients[client_s].dataBuffer.len = 1;
+	DWORD flags = 0; // 리시브는 무조건 플레그 필요 꼭 기억!
+	WSARecv(client_s, &clients[client_s].dataBuffer, 1, 0, &flags, overlapped, recv_callback);
+}
+
+int main()
+{
+	srand(time(NULL));
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 2), &WSAData);
+	SOCKET listenSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(SOCKADDR_IN));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(SERVER_PORT);
+	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	::bind(listenSocket, (struct sockaddr*)&serverAddr, sizeof(SOCKADDR_IN)); // c++11에 bind 키워드가 있음
+	// 키워드가 아니라 함수임을 알리는 용도
+	listen(listenSocket, 10);
+	SOCKADDR_IN clientAddr;
+	int addrLen = sizeof(SOCKADDR_IN);
+	memset(&clientAddr, 0, addrLen);
+
+	while (true)
+	{
+		SOCKET clientSocket = WSAAccept(listenSocket, (struct sockaddr*)&clientAddr, &addrLen, NULL, NULL);
+		clients[clientSocket] = SESSION{};
+		clients[clientSocket].socket = clientSocket;
+		clients[clientSocket].pos.x = (rand() % 8) * 60;
+		clients[clientSocket].pos.y = (rand() % 8) * 60;
+		clients[clientSocket].dataBuffer.len = sizeof(clients[clientSocket].pos);
+		Vec2 n_pos;
+		n_pos.x = clients[clientSocket].pos.x;
+		n_pos.y = clients[clientSocket].pos.y;
+		clients[clientSocket].dataBuffer.buf = reinterpret_cast<char*>(&n_pos); // 한 세션 생성 끝
+		clients[clientSocket].dataBuffer.len = sizeof(n_pos); // 한 세션 생성 끝
+		memset(&clients[clientSocket].overlapped, 0, sizeof(WSAOVERLAPPED));
+		cout << "New Client [" << clientSocket << "] connected!\n";
+		WSASend(clients[clientSocket].socket, &clients[clientSocket].dataBuffer, 1, NULL,
+			0, &(clients[clientSocket].overlapped), send_callback);		
+	}
+	closesocket(listenSocket);
+	WSACleanup();
+}
+
